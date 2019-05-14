ce947c30cd772438644e46b3b8e8cf0e1ce68002
zhangyuexu <zhangyuexu@xueqiu.com>
2019-05-14 10:26:35 +0800

    增加注释

--- src/main/java/com/zyx/grpc/StudentServiceImpl.java
+++ src/main/java/com/zyx/grpc/StudentServiceImpl.java
@@ -6,7 +6,7 @@ import io.grpc.stub.StreamObserver;
 import java.util.UUID;
 
 public class StudentServiceImpl extends StudentServiceGrpc.StudentServiceImplBase {
-
+    //第一种，客户端和服务端都是单个
     @Override
     public void getRealNameByUsername(MyRequest request, StreamObserver<MyResponse> responseObserver) {
         System.out.println("接收到客户端信息："+request.getUsername());
@@ -23,6 +23,7 @@ public class StudentServiceImpl extends StudentServiceGrpc.StudentServiceImplBas
         //responseObserver.onError();
     }
 
+    //第二种，客户端向服务端发送单个的数据请求，服务器返回流式的数据响应.
     @Override
     public void getStudentsByAge(StudentRequest request, StreamObserver<StudentResponse> responseObserver) {
         System.out.println("接收到客户端信息："+request.getAge());
@@ -36,7 +37,7 @@ public class StudentServiceImpl extends StudentServiceGrpc.StudentServiceImplBas
 
     }
 
-
+    //第三种，客户端向服务端发送流式的请求，服务端会返回一个(单个)
     @Override
     public StreamObserver<StudentRequest> getStudentsWrapperByAges(StreamObserver<StudentResponseList> responseObserver) {
 
@@ -70,6 +71,7 @@ public class StudentServiceImpl extends StudentServiceGrpc.StudentServiceImplBas
     }
 
 
+    //第四种，客户端与服务器端都是以流式的方式把数据发送给对方(双向的)
     @Override
     public StreamObserver<StreamRequest> biTalk(StreamObserver<StreamResponse> responseObserver) {
         return new StreamObserver<StreamRequest>() {
